66fbbde7ea98da413ee87faa417e0f280fbeb422
SamuelParrad0 <samuel.parrado01@gmail.com>
2026-02-25 10:00:08 -0500

    Create producto.controller.js

---
+++ backend/controllers/producto.controller.js
@@ -0,0 +1,14 @@
+/**
+ * Controlador de productos
+ * maneja las operaciones crud y activar y desactivar productos
+ * Solo accesible por administradores
+ */
+
+/**
+ * Importar modelos
+ */
+
+const Producto = require('../models/Producto');
+const Subcategoria = require('../models/Subcategoria');
+const Categoria = require('../models/Categoria');
+
